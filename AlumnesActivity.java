a19d94872bfb3d1bb9f92ad8293a69acd6da9988
Aleix Reverté <Aleix Revert>
2016-05-14 12:49:55 +0200

    Parte de Edu

--- AlumnesActivity.java
+++ AlumnesActivity.java
@@ -3,6 +3,8 @@ package com.example.ls31084.practicaprojectes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+//Parte de Edu
+
 public class AlumnesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
